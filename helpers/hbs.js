162b4c22c0d2377dbd6f690769cb7af4ade79828
amarbessedik <amar.bessedik@gmail.com>
2020-06-24 16:36:44 -0400

    Story editing capability added

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -16,5 +16,29 @@ module.exports = {
     },
     stripTags: (input) =>{
         return input.replace(/<(?:.|\n)*?>/gm, '');
+    },
+    editIcon: (storyUser, loggedUser, storyId, floating = true)=>{
+        if(storyUser._id.toString() == loggedUser._id.toString()){
+            if(floating){
+              return `<a href="/stories/edit/${storyId}" class="btn-floating halway-fab transparent"><i class="fas fa-edit fa-small"></i></a>`
+            }else{
+               return `<a href="/stories/edit/${storyId}"><i class="fas- fa-edit"></i>`
+            }
+        }else{
+            return ''
+        }
+    },
+
+    select: (selected, options) =>{
+        return options
+        .fn(this)
+        .replace(
+            new RegExp(' value="' + selected + '"'),
+            '$& selected="selected"'
+        )
+        .replace(
+            new RegExp('>' + selected + '</option>'),
+            ' selected="selected"$&'
+        )
     }
 }
--- routes/stories.js
+++ routes/stories.js
@@ -4,7 +4,7 @@ const { ensureAuth } = require("../middleware/auth");
 
 const Story = require("../models/Story");
 
-// @desc Login/Landing Page
+// @desc Show Add Page
 // @route GET to the route '/stories/add'
 router.get("/add", ensureAuth, (req, res) => {
   res.render("stories/add");
@@ -38,6 +38,43 @@ router.get("/", ensureAuth, async (req, res) => {
   } catch (err) {
     console.error(err);
     res.render("error/500");
+  }
+});
+
+// @desc Show the Edit Page
+// @route GET to the route '/stories/edit/:id'
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  const story = await Story.findOne({
+    _id: req.params.id
+  }).lean();
+
+  if(!story){
+    res.render('error/404')
+  }
+  if(story.user != req.user.id){
+    res.redirect('/stories');
+  }else{
+    res.render('stories/edit', {
+      story,
+    })
+  }
+});
+
+// @desc Update story
+// @route PUT to the route '/stories/:id'
+router.put("/:id", ensureAuth, async (req, res) => {
+  let story = await Story.findById(req.params.id).lean();
+  if(!story){
+    return res.render('error/404');
+  }
+  if (story.user != req.user.id) {
+    res.redirect("/stories");
+  } else {
+    story = await Story.findOneAndUpdate({_id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+    })
+    res.redirect('/dashboard');
   }
 });
 
